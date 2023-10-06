c831bda4af9c6027250b03f778fdd27fcfafec37
Fernando Espíndola <fernando.espindola@adp.com>
2023-10-06 16:55:07 -0300

    Added some adjustments to the bufferline plugin

--- lua/fernandoe/plugins/bufferline.lua
+++ lua/fernandoe/plugins/bufferline.lua
@@ -1,19 +1,22 @@
 return {
     "https://github.com/akinsho/bufferline.nvim",
     dependencies = {
-        "https://github.com/nvim-tree/nvim-web-devicons"
+        "https://github.com/nvim-tree/nvim-web-devicons",
     },
     version = "*",
     opts = {
         options = {
             mode = "buffers",
-            separator_style = "slant",
-            offsets = {{
-                filetype = "NvimTree",
-                text = "File Explorer",
-                highlight = "Directory",
-                text_align = "left"
-            }},
+            separator_style = "thick",
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    highlight = "Directory",
+                    separator = true,
+                    text_align = "left",
+                },
+            },
         },
     },
 }
