6ef04ae418fc759c4989265ed14841675d6eb323
Fernando Espíndola <fer.esp@gmail.com>
2023-10-09 00:17:22 -0300

    Configured the code folding using treesiter

--- lua/fernandoe/plugins/nvim-treesitter.lua
+++ lua/fernandoe/plugins/nvim-treesitter.lua
@@ -27,5 +27,10 @@ return {
             },
             auto_install = true,
         })
+
+        vim.opt.foldenable = false
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
     end,
 }
