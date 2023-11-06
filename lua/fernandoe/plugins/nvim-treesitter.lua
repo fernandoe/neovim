54b6f22c758c41e7fa75f0485b7ce5ecca32d221
Fernando Espíndola <fernando.espindola@adp.com>
2023-11-06 20:17:19 -0300

    Fold configuration.

--- lua/fernandoe/plugins/nvim-treesitter.lua
+++ lua/fernandoe/plugins/nvim-treesitter.lua
@@ -1,37 +1,32 @@
 return {
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local treesiter = require("nvim-treesitter.configs")
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local treesiter = require("nvim-treesitter.configs")
 
-        treesiter.setup({
-            highlight = { enable = true },
-            indent = { enable = true },
-            autotag = { enable = true },
-            ensure_installed = {
-                "bash",
-                "css",
-                "dockerfile",
-                "gitignore",
-                "html",
-                "http",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "query",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-            auto_install = true,
-        })
-
-        vim.opt.foldenable = false
-        vim.opt.foldmethod = "expr"
-        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
-    end,
+    treesiter.setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "html",
+        "http",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "query",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      auto_install = true,
+    })
+  end,
 }
---
+++ lua/fernandoe/plugins/nvim-ufo.lua
@@ -0,0 +1,50 @@
+return {
+  "https://github.com/kevinhwang91/nvim-ufo",
+  dependencies = {
+    "https://github.com/kevinhwang91/promise-async",
+    {
+      "https://github.com/luukvbaal/statuscol.nvim",
+      config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+          relculright = true,
+          segments = {
+            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+            { text = { "%s" }, click = "v:lua.ScSa" },
+            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          },
+        })
+      end,
+    },
+  },
+  event = "BufReadPost",
+  config = function()
+    vim.o.foldcolumn = "1" -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
+    -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+    vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+
+    -- Option 2: nvim lsp as LSP client
+    -- Tell the server the capability of foldingRange,
+    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    for _, ls in ipairs(language_servers) do
+      require("lspconfig")[ls].setup({
+        capabilities = capabilities,
+        -- you can add other fields for setting up lsp server in this table
+      })
+    end
+    require("ufo").setup({})
+  end,
+}
