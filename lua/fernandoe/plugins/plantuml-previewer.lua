8f64c83f1e65e62eb841aec6ba0916c4bd61f94c
Fernando Espíndola <fer.esp@gmail.com>
2023-11-05 23:50:36 -0300

    Add plantuml previewer

---
+++ lua/fernandoe/plugins/plantuml-previewer.lua
@@ -0,0 +1,7 @@
+return {
+    "https://github.com/weirongxu/plantuml-previewer.vim",
+    dependencies = {
+        "https://github.com/tyru/open-browser.vim",
+        "https://github.com/aklt/plantuml-syntax",
+    },
+}
