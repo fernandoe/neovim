f65ebeeacc798481fe53b59b00b71bcbb0dcf266
Fernando Espíndola <fernando.espindola@adp.com>
2023-11-09 18:15:47 -0300

    Add copilot-status plugin and update all others plugins

---
+++ lua/fernandoe/plugins/copilot-status.lua
@@ -0,0 +1,6 @@
+return {
+  "https://github.com/jonahgoldwastaken/copilot-status.nvim",
+  lazy = true,
+  event = "BufReadPost",
+  opts = {},
+}
--- lua/fernandoe/plugins/lualine.lua
+++ lua/fernandoe/plugins/lualine.lua
@@ -1,30 +1,36 @@
 return {
-    "https://github.com/nvim-lualine/lualine.nvim",
-    dependencies = {
-        "https://github.com/nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      local lualine = require("lualine")
-      local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+  "https://github.com/nvim-lualine/lualine.nvim",
+  dependencies = {
+    "https://github.com/nvim-tree/nvim-web-devicons",
+    "https://github.com/jonahgoldwastaken/copilot-status.nvim",
+  },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-      -- configure lualine with modified theme
-      lualine.setup({
-        options = {
-          -- theme = my_lualine_theme,
-          theme = 'tokyonight',
-        },
-        sections = {
-          lualine_x = {
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-              color = { fg = "#ff9e64" },
-            },
-            { "encoding" },
-            { "fileformat" },
-            { "filetype" },
+    local copilot_status = function()
+      return require("copilot_status").status_string()
+    end
+
+    -- configure lualine with modified theme
+    lualine.setup({
+      options = {
+        -- theme = my_lualine_theme,
+        theme = "tokyonight",
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
           },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+          { copilot_status },
         },
-      })
-    end,
-  }
+      },
+    })
+  end,
+}
