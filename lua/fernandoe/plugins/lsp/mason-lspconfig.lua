6967fd7977e1075de10e336feb0ade849c618487
Fernando Espíndola <fernando.espindola@adp.com>
2024-01-04 12:09:18 -0300

    Add none-ls as a formating and linting.

--- lua/fernandoe/plugins/lsp/mason-lspconfig.lua
+++ lua/fernandoe/plugins/lsp/mason-lspconfig.lua
@@ -1,24 +1,25 @@
 return {
-  "https://github.com/williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "https://github.com/williamboman/mason.nvim",
-  },
-  opts = {
-    ensure_installed = {
-      "ansiblels",
-      "bashls",
-      "cssls",
-      "docker_compose_language_service",
-      "gopls",
-      "html",
-      "lua_ls",
-      "marksman", -- Markdown
-      "pyright",
-      "tailwindcss",
-      "terraformls",
-      "tflint",
-      "tsserver",
+    "https://github.com/williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        "https://github.com/williamboman/mason.nvim",
     },
-    automatic_installation = true,
-  },
+    opts = {
+        ensure_installed = {
+            "ansiblels",
+            "bashls",
+            "cssls",
+            "docker_compose_language_service",
+            "gopls",
+            "html",
+            "lua_ls",
+            "marksman", -- Markdown
+            "pyright",
+            "tailwindcss",
+            "terraformls",
+            "tflint",
+            "tsserver",
+            "rubocop",
+        },
+        automatic_installation = true,
+    },
 }
