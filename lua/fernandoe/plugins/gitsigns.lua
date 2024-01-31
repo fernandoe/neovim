bd28c20d5658cbe36128aa91f4b0283637c4bed2
Fernando Espíndola <fernando.espindola@adp.com>
2024-01-31 02:38:39 -0300

    Add some

--- lua/fernandoe/plugins/gitsigns.lua
+++ lua/fernandoe/plugins/gitsigns.lua
@@ -1,5 +1,8 @@
 return {
-  "https://github.com/lewis6991/gitsigns.nvim",
-  enabled = true,
-  opts = {},
+    "https://github.com/lewis6991/gitsigns.nvim",
+    enabled = true,
+    config = function()
+        require("gitsigns").setup()
+        vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", { desc = "Show git diff" })
+    end,
 }
