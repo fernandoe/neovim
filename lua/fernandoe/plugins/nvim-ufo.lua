c4e75e223ace6e896c81368d8273e6a3fc329b68
Fernando Espíndola <fernando.espindola@adp.com>
2024-02-16 02:01:06 -0300

    2024.02.15

--- lua/fernandoe/plugins/nvim-ufo.lua
+++ lua/fernandoe/plugins/nvim-ufo.lua
@@ -1,51 +1,45 @@
 return {
-  "https://github.com/kevinhwang91/nvim-ufo",
-  enabled = false,
-  dependencies = {
-    "https://github.com/kevinhwang91/promise-async",
-    {
-      "https://github.com/luukvbaal/statuscol.nvim",
-      config = function()
-        local builtin = require("statuscol.builtin")
-        require("statuscol").setup({
-          relculright = true,
-          segments = {
-            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-            { text = { "%s" }, click = "v:lua.ScSa" },
-            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-          },
-        })
-      end,
+    "https://github.com/kevinhwang91/nvim-ufo",
+    dependencies = {
+        "https://github.com/kevinhwang91/promise-async",
+        "https://github.com/luukvbaal/statuscol.nvim",
     },
-  },
-  event = "BufReadPost",
-  config = function()
-    vim.o.foldcolumn = "1" -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
-    -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    event = "BufReadPost",
+    enabled = false,
+    config = function()
+        require("statuscol").setup({})
 
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+        require("ufo").setup({})
+    end,
+    -- config = function()
+    -- vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
+    -- -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    --
+    -- -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    -- vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+    -- vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+    --
+    -- -- Option 2: nvim lsp as LSP client
+    -- -- Tell the server the capability of foldingRange,
+    -- -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities.textDocument.foldingRange = {
+    --     dynamicRegistration = false,
+    --     lineFoldingOnly = true,
+    -- }
+    -- local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    -- for _, ls in ipairs(language_servers) do
+    --     require("lspconfig")[ls].setup({
+    --         capabilities = capabilities,
+    --         -- you can add other fields for setting up lsp server in this table
+    --     })
+    -- end
+    -- require("ufo").setup({})
 
-    -- Option 2: nvim lsp as LSP client
-    -- Tell the server the capability of foldingRange,
-    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
-    }
-    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-    for _, ls in ipairs(language_servers) do
-      require("lspconfig")[ls].setup({
-        capabilities = capabilities,
-        -- you can add other fields for setting up lsp server in this table
-      })
-    end
-    require("ufo").setup({})
-  end,
+    -- require("ufo").setup({
+    --     provider_selector = function(_bufnr, _filetype, _buftype)
+    --         return { "treesitter", "indent" }
+    --     end,
+    -- })
+    -- end,
 }
