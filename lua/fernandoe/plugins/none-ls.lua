b3aaa42f955bc294f2d037534cf9f5bb603c2870
Fernando Espíndola <fernando.espindola@adp.com>
2024-03-21 19:40:18 -0300

    2024.03.21

--- lua/fernandoe/plugins/none-ls.lua
+++ lua/fernandoe/plugins/none-ls.lua
@@ -1,42 +1,47 @@
 return {
     "https://github.com/nvimtools/none-ls.nvim",
+    dependencies = { "https://github.com/nvim-lua/plenary.nvim" },
     enabled = true,
     config = function()
         local null_ls = require("null-ls")
 
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
+                -- null_ls.builtins.formatting.stylua,
+                --
+                null_ls.builtins.formatting.eslint,
+                null_ls.builtins.diagnostics.eslint,
+                --
+                -- null_ls.builtins.formatting.prettierd,
+                -- null_ls.builtins.formatting.prettier.with({
+                --     disabled_filetypes = { "json" },
+                -- }),
+                -- -- null_ls.builtins.diagnostics.eslint_d,
+                --
+                -- null_ls.builtins.completion.spell,
+                --
+                -- null_ls.builtins.formatting.black,
+                -- null_ls.builtins.formatting.isort,
+                --
+                -- null_ls.builtins.diagnostics.rubocop,
+                -- null_ls.builtins.formatting.rubocop,
 
-                null_ls.builtins.formatting.prettier.with({
-                    disabled_filetypes = { "json" },
-                }),
-                -- null_ls.builtins.diagnostics.eslint_d,
-
-                null_ls.builtins.completion.spell,
-
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-
-                null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.formatting.rubocop,
-
-                null_ls.builtins.formatting.biome.with({
-                    filetypes = {
-                        "json",
-                    },
-                    args = {
-                        "check",
-                        "--apply",
-                        "--indent-style=space",
-                        "--indent-width=4",
-                        "--linter-enabled=true",
-                        "--formatter-enabled=true",
-                        "--organize-imports-enabled=true",
-                        "--skip-errors",
-                        "$FILENAME",
-                    },
-                }),
+                -- null_ls.builtins.formatting.biome.with({
+                --     filetypes = {
+                --         "json",
+                --     },
+                --     args = {
+                --         "check",
+                --         "--apply",
+                --         "--indent-style=space",
+                --         "--indent-width=4",
+                --         "--linter-enabled=true",
+                --         "--formatter-enabled=true",
+                --         "--organize-imports-enabled=true",
+                --         "--skip-errors",
+                --         "$FILENAME",
+                --     },
+                -- }),
             },
         })
 
