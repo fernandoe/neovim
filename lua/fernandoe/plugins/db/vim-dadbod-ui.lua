9edd902f18656ad770dd572839e4b8e7e92b016f
Fernando Espíndola <fer.esp@gmail.com>
2024-05-14 17:27:10 -0300

    Add dbconfiguration

---
+++ lua/fernandoe/plugins/db/vim-dadbod-ui.lua
@@ -0,0 +1,13 @@
+return {
+    "https://github.com/kristijanhusak/vim-dadbod-ui",
+    cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
+    },
+    init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+    end,
+}
--- lua/fernandoe/plugins/nvim-cmp.lua
+++ lua/fernandoe/plugins/nvim-cmp.lua
@@ -13,26 +13,15 @@ return {
     "https://github.com/onsails/lspkind.nvim",
     "https://github.com/rafamadriz/friendly-snippets", -- useful snippets
     "https://github.com/onsails/lspkind.nvim", -- vs-code like pictograms
+    "https://github.com/kristijanhusak/vim-dadbod-completion", -- for db autocompletion
   },
   config = function()
     local cmp = require("cmp")
-
-    -- local luasnip = require("luasnip")
-
-    local lspkind = require("lspkind")
-
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    -- require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      -- snippet = { -- configure how nvim-cmp interacts with snippet engine
-      --   expand = function(args)
-      --     luasnip.lsp_expand(args.body)
-      --   end,
-      -- },
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
@@ -49,10 +38,34 @@ return {
       -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        -- { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+        { name = "buffer" },
+        { name = "path" },
       }),
+    })
+
+    -- Setup up vim-dadbod
+    cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      },
+    })
+
+    --[[
+    -- local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    -- require("luasnip.loaders.from_vscode").lazy_load()
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      -- snippet = { -- configure how nvim-cmp interacts with snippet engine
+      --   expand = function(args)
+      --     luasnip.lsp_expand(args.body)
+      --   end,
+      -- },
+      -- sources for autocompletion
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
@@ -75,5 +88,8 @@ return {
         { name = "cmdline" },
       }),
     })
+
+]]
+    --
   end,
 }
