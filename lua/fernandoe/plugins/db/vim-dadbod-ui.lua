186794b55b7b5db36bb3d86f3c9b05a476268f11
Fernando Espíndola <fernando.espindola@adp.com>
2024-05-15 13:14:08 -0300

    2024.05.15

--- lua/fernandoe/plugins/db/vim-dadbod-ui.lua
+++ lua/fernandoe/plugins/db/vim-dadbod-ui.lua
@@ -1,13 +1,14 @@
 return {
-    "https://github.com/kristijanhusak/vim-dadbod-ui",
-    cmd = {
-        "DBUI",
-        "DBUIToggle",
-        "DBUIAddConnection",
-        "DBUIFindBuffer",
-    },
-    init = function()
-        -- Your DBUI configuration
-        vim.g.db_ui_use_nerd_fonts = 1
-    end,
+  "https://github.com/kristijanhusak/vim-dadbod-ui",
+  enabled = false,
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
 }
--- lua/fernandoe/plugins/db/vim-dadbod.lua
+++ lua/fernandoe/plugins/db/vim-dadbod.lua
@@ -1,5 +1,6 @@
 return {
   "https://github.com/tpope/vim-dadbod",
+  enabled = false,
   dependencies = {
     "https://github.com/kristijanhusak/vim-dadbod-ui",
     "https://github.com/kristijanhusak/vim-dadbod-completion",
