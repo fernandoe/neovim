1ff18c502365877fc8c260ac4a1c77b3f6ab6267
Fernando Espíndola <fer.esp@gmail.com>
2024-11-15 23:48:10 -0300

    COnfiguraçãao do bufferline

--- lua/fernandoe/plugins/nvim-notify.lua
+++ lua/fernandoe/plugins/nvim-notify.lua
@@ -1,6 +1,6 @@
 return {
-    "https://github.com/rcarriga/nvim-notify",
-    opts = {
-        background_colour = "#000000",
-    },
+  "https://github.com/rcarriga/nvim-notify",
+  opts = {
+    background_colour = "#000000",
+  },
 }
