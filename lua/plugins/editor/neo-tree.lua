11628c3336b371da0cb52753b10e88d2e4d5913a
Fernando Espíndola <fer.esp@gmail.com>
2025-01-18 01:58:55 -0300

    Adding new configuration

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -1,8 +1,14 @@
 return {
-	"https://github.com/nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-	  "https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
-	"https://github.com/MunifTanjim/nui.nvim",
-	},
+  "https://github.com/nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree",
+  dependencies = {
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/nvim-tree/nvim-web-devicons",
+    "https://github.com/MunifTanjim/nui.nvim",
+  },
+  keys = {
+		{ "<leader>fe", "<cmd>:Neotree filesystem toggle<cr>", desc = "File explorer (NeoTree)" },
+		{ "<leader>fE", "<cmd>:Neotree filesystem reveal toggle<cr>", desc = "File explorer (NeoTree)" },
+		{ "<leader>ge", "<cmd>:Neotree git_status toggle<cr>", desc = "Git explorer (NeoTree)" },
+  },
 }
