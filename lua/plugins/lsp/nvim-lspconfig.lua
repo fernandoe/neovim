f2f3cd0565d5457585f494e76227f6d1f4376178
Fernando Espíndola <fer.esp@gmail.com>
2025-01-18 00:45:52 -0300

    Adicionando Telescope

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "https://github.com/hrsh7th/cmp-nvim-lsp",
   },
-  enabled = true,
+  enabled = false,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
