3111d9f03333efd16a26fb74594bfc9a800b4564
Fernando Espíndola <fer.esp@gmail.com>
2025-01-20 14:30:32 -0300

    wip

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -5,3 +5,15 @@ vim.o.clipboard = "unnamed,unnamedplus"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.splitright = true       -- Vertical split to the right
+vim.opt.splitbelow = true       -- Horizontal split to the bottom
+vim.opt.colorcolumn = "100"     -- Show vertical line to help on write code
