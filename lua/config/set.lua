0eaf993a0a353aeb45c6c6135b85beb4e6730f4f
Fernando Espíndola <fernando.espindola@adp.com>
2025-01-22 01:00:17 -0300

    tmux navigator

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -17,3 +17,10 @@ vim.opt.incsearch = true
 vim.opt.splitright = true       -- Vertical split to the right
 vim.opt.splitbelow = true       -- Horizontal split to the bottom
 vim.opt.colorcolumn = "100"     -- Show vertical line to help on write code
+
+
+-- https://github.com/kevinhwang91/nvim-ufo
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
