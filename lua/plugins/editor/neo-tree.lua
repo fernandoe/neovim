d4019378a66168975b6028fc2bc00960f1b7f2c0
Fernando Espíndola <fernando.espindola@adp.com>
2025-01-22 23:21:40 -0300

    wip

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -7,8 +7,11 @@ return {
     "https://github.com/MunifTanjim/nui.nvim",
   },
   keys = {
-		{ "<leader>fe", "<cmd>:Neotree filesystem toggle<cr>", desc = "File explorer (NeoTree)" },
-		{ "<leader>fE", "<cmd>:Neotree filesystem reveal toggle<cr>", desc = "File explorer (NeoTree)" },
-		{ "<leader>ge", "<cmd>:Neotree git_status toggle<cr>", desc = "Git explorer (NeoTree)" },
+    { "<leader>fe", "<cmd>:Neotree filesystem toggle<cr>", desc = "File explorer (NeoTree)" },
+    { "<leader>fE", "<cmd>:Neotree filesystem reveal toggle<cr>", desc = "File explorer (NeoTree)" },
+    { "<leader>ge", "<cmd>:Neotree git_status toggle<cr>", desc = "Git explorer (NeoTree)" },
+  },
+  opts = {
+    close_if_last_window = true,
   },
 }
