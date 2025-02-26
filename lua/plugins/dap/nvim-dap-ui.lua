9ee65e958a2759093151aa387d3ce19b98f0ca38
Fernando Espíndola <fernando.espindola@adp.com>
2025-02-26 10:20:20 -0300

    Adding cheatsheet for my configuration

--- lua/plugins/dap/nvim-dap-ui.lua
+++ lua/plugins/dap/nvim-dap-ui.lua
@@ -1,8 +1,28 @@
 return {
   "https://github.com/rcarriga/nvim-dap-ui",
-  enabled = true,
   dependencies = {
     "https://github.com/mfussenegger/nvim-dap",
     "https://github.com/nvim-neotest/nvim-nio",
   },
+  keys = {
+    { "<leader>ui", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+  },
+  enabled = true,
+  event = "VeryLazy",
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dapui").setup({})
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+  end,
 }
