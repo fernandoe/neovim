ad0488d687ac03dcb164fad44d0c9cd72d7a1bda
Fernando Espíndola <fernando.espindola@adp.com>
2025-02-26 09:57:01 -0300

    Adding partially the dap configuration

---
+++ lua/plugins/dap/nvim-dap-ui.lua
@@ -0,0 +1,8 @@
+return {
+  "https://github.com/rcarriga/nvim-dap-ui",
+  enabled = true,
+  dependencies = {
+    "https://github.com/mfussenegger/nvim-dap",
+    "https://github.com/nvim-neotest/nvim-nio",
+  },
+}
---
+++ lua/plugins/dap/nvim-dap-virtual-text.lua
@@ -0,0 +1,8 @@
+return {
+  "https://github.com/theHamsta/nvim-dap-virtual-text",
+  enabled = true,
+  dependencies = {
+    "https://github.com/mfussenegger/nvim-dap",
+  },
+  opts = {},
+}
