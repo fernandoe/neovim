585118f7cb2f949a97ea366495ab9f0c57c3f4f4
Fernando Espíndola <fernando.espindola@adp.com>
2025-02-27 19:57:44 -0300

    wip

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "https://github.com/hrsh7th/cmp-nvim-lsp",
   },
-  enabled = false,
+  enabled = true,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
