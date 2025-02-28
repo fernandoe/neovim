893346755f32ac31fc535a732c707a9bb7da18c3
Fernando Espíndola <fernando.espindola@adp.com>
2025-02-27 22:59:42 -0300

    Configuraçãõ dos tabs

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,10 +2,19 @@ return {
   "https://github.com/neovim/nvim-lspconfig",
   dependencies = {
     "https://github.com/hrsh7th/cmp-nvim-lsp",
+    "https://github.com/hrsh7th/nvim-cmp",
   },
   enabled = true,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    local lspconfig = require("lspconfig")
+
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+      -- Identifica o diretório raiz do projeto
+      -- root_dir = lspconfig.util.root_pattern("package.json", ".git"),
+    })
   end,
 }
