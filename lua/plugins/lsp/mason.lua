1f1888cff118bd403cad6381c5a3751cd6d58adc
Fernando Espíndola <fernando.espindola@adp.com>
2025-03-08 20:31:55 -0300

    wip

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -2,6 +2,9 @@ return {
   "https://github.com/williamboman/mason.nvim",
   enabled = true,
   opts = {
+    ensure_installed = {
+      "typescript-language-server",
+    },
     ui = {
       icons = {
         enabled = true,
