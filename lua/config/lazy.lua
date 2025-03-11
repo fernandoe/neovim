41a9915c0a406837856b6cb2a4624443f45a0dde
Fernando Espíndola <fernando.espindola@adp.com>
2025-03-10 22:43:10 -0300

    wip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,4 +34,5 @@ require("lazy").setup({
 -- vim.cmd('colorscheme darcula')
 -- vim.cmd([[colorscheme tokyonight-moon]])
 -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-vim.cmd([[colorscheme catppuccin-macchiato]])
+-- vim.cmd([[colorscheme catppuccin-macchiato]])
+vim.cmd([[colorscheme kanagawa]])
---
+++ lua/plugins/themes/kanagawa.lua
@@ -0,0 +1,6 @@
+return {
+  "https://github.com/rebelot/kanagawa.nvim",
+  opts = {
+    transparent = false,
+  },
+}
