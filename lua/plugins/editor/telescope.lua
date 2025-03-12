8ab3776cba3ea095fa25c9a6fcdfe2f2e71f05ec
Fernando Espíndola <fernando.espindola@adp.com>
2025-03-12 17:39:36 -0300

    wip

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -9,6 +9,8 @@ return {
     { "<leader>ff", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", desc = "Find files" },
     { "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Find string in files" },
     { "<leader>fl", "<cmd>Telescope resume<cr>", desc = "Open last find window" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Open Neovim help tags" },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Lists normal mode keymappings" },
   },
   config = function()
     local telescope = require("telescope")
@@ -39,6 +41,5 @@ return {
     -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in files" })
     keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Find string in files" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-    keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find tags" })
   end,
 }
