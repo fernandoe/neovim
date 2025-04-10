cfc584f228138bd1272b066ccf50f5d3b4563171
Fernando Espíndola <fer.esp@gmail.com>
2025-04-10 16:41:04 -0300

    adding specre

--- lua/plugins/coding/nvim-spectre.lua
+++ lua/plugins/coding/nvim-spectre.lua
@@ -1,4 +1,9 @@
 return {
   "https://github.com/nvim-pack/nvim-spectre",
+  keys = {
+    { "<leader>S", "<cmd>:Spectre<cr>", desc = "Search and Replace" },
+    { "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", desc = "Search current word" },
+    { "<leader>sf", "<cmd>lua require('spectre').open_file_search({select_word=true})<cr>", desc = "Search in file" },
+  },
   opts = {},
 }
